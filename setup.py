198b45d921c072f8b5c549b8b1f54fde4efa9571
Rosalie-A <61372066+Rosalie-A@users.noreply.github.com>
2025-02-10 11:17:51 -0500

    Seventh

--- setup.py
+++ setup.py
@@ -4,5 +4,17 @@ from setuptools import setup  # type: ignore
 if __name__ == "__main__":
     setup(packages=[
         "FreeEnt",
-        "FreeEnt.f4c"
+        "FreeEnt.f4c",
+        "FreeEnt.f4c.ff4bin",
+        "FreeEnt.f4c.ff4struct",
+        "FreeEnt.assets",
+        "FreeEnt.assets.db",
+        "FreeEnt.assets.encounters",
+        "FreeEnt.assets.fashion",
+        "FreeEnt.assets.item_info",
+        "FreeEnt.assets.title",
+        "FreeEnt.binary_patches",
+        "FreeEnt.scripts",
+        "FreeEnt.scripts.hobs_spells",
+        "FreeEnt.scripts.wacky",
     ], use_scm_version=True)
